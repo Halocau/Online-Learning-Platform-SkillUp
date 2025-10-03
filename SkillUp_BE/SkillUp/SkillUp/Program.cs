129cf798b89b20a28c04b648c078625ac4675eb3
TranBaBinh <binhtbhe173244@fpt.edu.vn>
2025-10-04 00:44:48 +0700

    signal

--- SkillUp_BE/SkillUp/SkillUp/Program.cs
+++ SkillUp_BE/SkillUp/SkillUp/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<SkillUpContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddSignalR(); //add signalR
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
