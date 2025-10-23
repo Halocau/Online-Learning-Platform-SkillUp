b144ee0e5b154d300d66d487f07dc29e5e8af813
TranBaBinh <binhtbhe173244@fpt.edu.vn>
2025-10-23 15:33:46 +0700

    api change password

---
+++ SkillUp_BE/SkillUp/BussinessObjects/DTOs/Auth/ChangePasswordRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillUp.BussinessObjects.DTOs.Auth
+{
+    public class ChangePasswordRequestDto
+    {
+        [Required(ErrorMessage = "Mật khẩu cũ là bắt buộc")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu mới là bắt buộc")]
+        [MinLength(6, ErrorMessage = "Mật khẩu mới phải có ít nhất 6 ký tự")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Xác nhận mật khẩu là bắt buộc")]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu mới và mật khẩu xác nhận không khớp")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
