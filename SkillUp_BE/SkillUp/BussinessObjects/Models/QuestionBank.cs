cce9288a5d4a62b08e93b3e9608bd0fcfea10f1d
Halocau <buitienquat2k3@gmail.com>
2025-11-12 23:34:51 +0700

    update model

--- SkillUp_BE/SkillUp/BussinessObjects/Models/QuestionBank.cs
+++ SkillUp_BE/SkillUp/BussinessObjects/Models/QuestionBank.cs
@@ -23,6 +23,8 @@ public partial class QuestionBank
 
     public string? Image { get; set; }
 
+    public bool? IsHidden { get; set; }
+
     public virtual ICollection<AnswerBank> AnswerBanks { get; set; } = new List<AnswerBank>();
 
     public virtual Lecturer Lecturer { get; set; } = null!;
