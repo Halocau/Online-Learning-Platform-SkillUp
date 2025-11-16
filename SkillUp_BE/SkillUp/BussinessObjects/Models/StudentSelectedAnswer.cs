b5a63c41bc91539aacc374d35ce256bb720c342a
Halocau <buitienquat2k3@gmail.com>
2025-11-16 22:37:16 +0700

    update model

---
+++ SkillUp_BE/SkillUp/BussinessObjects/Models/StudentSelectedAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkillUp.BussinessObjects.Models;
+
+public partial class StudentSelectedAnswer
+{
+    public Guid AnswerBankId { get; set; }
+
+    public Guid QuizAnswerSubmissionId { get; set; }
+
+    public Guid Id { get; set; }
+
+    public virtual AnswerBank AnswerBank { get; set; } = null!;
+
+    public virtual QuizAnswerSubmission QuizAnswerSubmission { get; set; } = null!;
+}
