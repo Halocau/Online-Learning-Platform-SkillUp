be1fc5647036666be6e24431e23ae36266345aa5
Tran Tuan Hai <169177287+HaiTTHE186621@users.noreply.github.com>
2025-11-21 14:13:45 +0700

    backend banner done

--- SkillUp_BE/SkillUp/Repositories/Implementations/BannerRepository.cs
+++ SkillUp_BE/SkillUp/Repositories/Implementations/BannerRepository.cs
@@ -13,11 +13,37 @@ namespace SkillUp.Repositories.Implementations
             _context = context;
         }
 
-        public async Task<List<Banner>> GetActiveBannersAsync()
+		public async Task<Banner> CreateBannerAsync(Banner banner)
+		{
+			await _context.Banners.AddAsync(banner);
+			return banner;
+		}
+
+		public async Task<List<Banner>> GetActiveBannersAsync()
         {
             return await _context.Banners
                 .Where(b => b.IsActive == true)
                 .ToListAsync();
         }
-    }
+
+		public async Task<List<Banner>> GetAllBannersAsync()
+		{
+			return await _context.Banners.ToListAsync();
+		}
+
+		public async Task<Banner?> GetBannerByIdAsync(int id)
+		{
+			return await _context.Banners.FindAsync(id);
+		}
+
+		public async Task<bool> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync() > 0;
+		}
+
+		public void UpdateBanner(Banner banner)
+		{
+			_context.Banners.Update(banner);
+		}
+	}
 }
