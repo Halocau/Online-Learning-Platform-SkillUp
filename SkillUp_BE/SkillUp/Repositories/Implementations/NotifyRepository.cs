e15bb155150816bfd8c2f54191c809cc0d974c4e
Vu Duc Hien <vuduchien865@gmail.com>
2025-11-23 22:13:01 +0700

    chuyen trang thai notify
    chuyen trang thai notify

--- SkillUp_BE/SkillUp/Repositories/Implementations/NotifyRepository.cs
+++ SkillUp_BE/SkillUp/Repositories/Implementations/NotifyRepository.cs
@@ -34,5 +34,30 @@
                     .OrderByDescending(n => n.CreatedAt)
                     .ToListAsync();
             }
+        public async Task<Notify?> GetByIdAsync(Guid id)
+        {
+            return await _context.Notifies.FindAsync(id);
         }
+
+        public async Task UpdateAsync(Notify notification)
+        {
+            _context.Notifies.Update(notification);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Notify>> GetUnreadByAccountIdAsync(Guid accountId)
+        {
+            // Lấy các thông báo của user mà Status là "Unread"
+            return await _context.Notifies
+                .Where(n => n.AccountId == accountId && n.Status == "Unread")
+                .ToListAsync();
+        }
+
+        public async Task UpdateRangeAsync(IEnumerable<Notify> notifications)
+        {
+            _context.Notifies.UpdateRange(notifications);
+            await _context.SaveChangesAsync();
+        }
+
+    }
     }
