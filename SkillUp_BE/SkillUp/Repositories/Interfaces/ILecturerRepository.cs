5cfebb43b222cc78044fa0d94d1e2168f6bf374e
Vu Duc Hien <vuduchien865@gmail.com>
2025-11-25 13:08:24 +0700

    Profile lecturer
    Profile lecturer

--- SkillUp_BE/SkillUp/Repositories/Interfaces/ILecturerRepository.cs
+++ SkillUp_BE/SkillUp/Repositories/Interfaces/ILecturerRepository.cs
@@ -10,5 +10,6 @@ namespace SkillUp.Repositories.Interfaces
         Task<Lecturer> AddAsync(Lecturer lecturer);
         Task<List<Lecturer>> GetAllLecturersAsync();
         Task<Lecturer?> GetLecturerByAccountIdAsync(Guid accountId);
+        Task<Lecturer?> GetLecturerByIdAsync(Guid lecturerId);
     }
 }
