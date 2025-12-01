ee420935ae7102ddf119c71f799684bcd09e96fa
Tran Tuan Hai <169177287+HaiTTHE186621@users.noreply.github.com>
2025-12-01 17:22:30 +0700

    api lecturer payroll list added

---
+++ SkillUp_BE/SkillUp/Controllers/PayrollController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkillUp.ExceptionHandling;
+using SkillUp.Services.Interfaces;
+
+namespace SkillUp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PayrollController : ControllerBase
+	{
+		private readonly IPayrollService _payrollService;
+		private readonly ICurrentUserService _currentUserService;
+		public PayrollController(IPayrollService payrollService, ICurrentUserService currentUserService)
+		{
+			_payrollService = payrollService;
+			_currentUserService = currentUserService;
+		}
+
+		[HttpGet("monthly-report")]
+		[Authorize]
+		public async Task<IActionResult> GetMonthlyReport([FromQuery] int month, [FromQuery] int year)
+		{
+			try
+			{
+				if (_currentUserService.RoleId != 1)
+				{
+					return Unauthorized(new APIReturn
+					{
+						code = 401,
+						message = "Bạn không phải là Admin!",
+						data = new List<object>()
+					});
+				}
+
+				if (month < 1 || month > 12)
+					return BadRequest(new APIReturn
+					{
+						code = 401,
+						message = "Tháng không hợp lệ!",
+						data = new List<object>()
+					}); ;
+
+				var report = await _payrollService.GenerateMonthlyPayrollReportAsync(month, year);
+
+				return Ok(new APIReturn
+				{
+					code = 200,
+					message = $"Lấy danh sách lương giảng viên tháng {month} năm {year} thành công",
+					data = { report }
+				});
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new APIReturn
+				{
+					code = 500,
+					message = "Có lỗi xảy ra: " + ex.Message,
+					data = new List<object>()
+				});
+			}
+		}
+	}
+}
---
+++ SkillUp_BE/SkillUp/Repositories/Interfaces/IPayrollRepository.cs
@@ -0,0 +1,9 @@
+﻿using SkillUp.BussinessObjects.DTOs.Salary;
+
+namespace SkillUp.Repositories.Interfaces
+{
+	public interface IPayrollRepository
+	{
+		Task<List<LecturerPayrollDto>> GetAllLecturersPayrollAsync(int month, int year);
+	}
+}
