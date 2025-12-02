9454bed3822a4078d11914a21e2fbe978318e350
Tran Tuan Hai <169177287+HaiTTHE186621@users.noreply.github.com>
2025-12-02 22:43:35 +0700

    added function check question used

--- SkillUp_BE/SkillUp/Repositories/Interfaces/IQuestionQuizRepository.cs
+++ SkillUp_BE/SkillUp/Repositories/Interfaces/IQuestionQuizRepository.cs
@@ -5,7 +5,8 @@ namespace SkillUp.Repositories.Interfaces
     public interface IQuestionQuizRepository
     {
         Task<QuestionQuiz?> GetLinkAsync(Guid quizId, Guid questionBankId);
+        Task<QuestionQuiz?> CheckQuestionUsed(Guid questionBankId);
 
-        void Update(QuestionQuiz questionQuiz);
+		void Update(QuestionQuiz questionQuiz);
     }
 }
